cf9c04625a5a148ab44668837579a0477fdfb6b1
HugoSetta <hugosetta@gmail.com>
2017-05-01 20:05:30 -0300

    Add files via upload

--- global.js
+++ global.js
@@ -1,3 +1,5 @@
+// Sun Mode ==========================================================================================
+
 var global_sunmode = false;
 
 function sunMode(){
@@ -8,3 +10,30 @@ function sunMode(){
         $("#stage").removeClass("sun-mode");
     }
 }
+// Parametros ==========================================================================================
+
+var global_params = {};
+
+function get_params(){
+    var url = window.location.toString();
+    var urlar = url.split("?");
+    if(urlar.length > 1){
+        var params = urlar[1].split("&");
+        for(var i=0; i<params.length; i++){
+            var pair = params[i].split("=");
+            if(pair.length > 1){
+                global_params[pair[0]] = pair[1];
+            }
+        }
+    }
+    
+}
+
+// inicialização ==========================================================================================
+$( document ).ready(function() {
+    get_params();
+    if(typeof global_params['hist'] != 'undefined'){
+        if(global_params['hist'])
+
+    }
+});
---
+++ hist_data.js
@@ -0,0 +1,52 @@
+// var hist_model = {
+// "id":{
+//     "nome":string,
+//     "fonte":[
+//         {
+//         "fonte_nome":string,
+//         "fonte_url":string,
+//         "fonte_ano":number
+//         }
+//     ],
+//     "autor":[
+//         string
+//     ],
+//     "qtdpalavras":number,
+//     "tags":[
+//         string
+//     ]
+// }
+// }
+
+var tags = [
+    "alegria",
+    "animais",
+    "arrependimento",
+    "briga",
+    "classicos",
+    "culpa",
+    "família",
+    "medo",
+    "menina",
+    "menino",
+    "mentira",
+    "moral",
+    "tristeza",
+    "verdade"
+];
+    
+var hist_index = {
+    1:{
+        "nome":"O Patinho Feio",
+        "fonte":[{
+            "fonte_nome":"www.qdivertido.com.br",
+            "fonte_url":"http://www.qdivertido.com.br/verconto.php?codigo=8",
+            "fonte_ano":2017
+        }],
+        "autor":["Hans Christian Andersen"],
+        "qtdpalavras":1564,
+        "tags":[
+            "tristeza","animais","classicos"
+        ]
+    }
+};
